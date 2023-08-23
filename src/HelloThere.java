b808164264616f2d03d30b8242fc81e8cf57d3cd
gustavopeixoto <gustavo.ogpeixoto@gmail.com>
2023-08-23 09:52:32 -0300

    segundo teste

--- src/HelloThere.java
+++ src/HelloThere.java
@@ -1,5 +1,7 @@
 public class HelloThere {
     public static void main(String[] args) {
         System.out.println("Hey there!!");
+        System.out.println("hahahahaha");
     }
 }
+
---
+++ src/TesteVariaveis.java
@@ -0,0 +1,9 @@
+public class TesteVariaveis {
+    public static void main(String[] args) {
+        int salario = 3500;
+
+        System.out.println(salario);
+
+
+    }
+}
